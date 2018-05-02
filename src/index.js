64844442aa9da602ff559ec499ed15e21bdc540f
Patrick Cullen <patrick.cullen@washpost.com>
2018-05-02 16:15:41 -0400

    added time to webpage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Hello World 4:14pm</h1>, document.getElementById('root'));
